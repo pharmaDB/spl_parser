f66af2095188241f7e0e3ddef4d6e52a32a834e3
Brett Bloethner <brett@wirehead.io>
2021-02-13 00:35:43 -0700

    add some comments

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@
 const fs = require('fs');
 import * as cheerio from 'cheerio';
 
+/**
+ * just an interface to enforce an object that contains unlimited key value pairs of which the value can be of any type
+ */
 interface OpenObject {
   [key: string]: any;
 }
 
+/**
+ *
+ * @param elements
+ */
 const toText = (elements: any) => {
     let ret = '';
     const len = elements.length;
@@ -22,6 +29,12 @@ const toText = (elements: any) => {
     return ret;
   };
 
+/**
+ * Parse out entires in the sections CSV file and map them to an object in key values pairs. The Resulting mapping lets
+ * the subsequent parsing functions know what sections to parse the XML SPL for and what to title the resulting
+ * sections in the JSON output.
+ * @param sectionsFilepath: the file path for the location of the sections.csv config file
+ */
 const parseSections = (sectionsFilepath: string): OpenObject => {
   // read the CSV and split it on line breaks
   const csv = fs.readFileSync(sectionsFilepath, 'utf-8');
@@ -44,13 +57,16 @@ const parseSections = (sectionsFilepath: string): OpenObject => {
   return codeToName;
 }
 
-const populateSectionsFromXml = (xmlSplString: string, jsonResponse: OpenObject, sectionsMap: OpenObject) => {
+/**
+ * Iterate through the jQuery enabled SPL document and parse out individual sections based on the standardized
+ * categories provided in the sections CSV config file. Parse out each categories body and add the category
+ * name:["body"] to the JSON response as a key value pair.
+ * @param $: A cheerio Root section object (ie XML document in a jQuery for Node wrapper)
+ * @param jsonResponse: the in-work jsonResponse that represents the XML SPL doc but in JSON format
+ * @param sectionsMap: mapping that defines which SPL REMS section definitions will be looked for while parsing the SPL
+ */
+const populateSectionsFromXml = ($: cheerio.Root, jsonResponse: OpenObject, sectionsMap: OpenObject) => {
   // load the xml into a JQuery for Node wrapper
-  const $ = cheerio.load(xmlSplString, {
-    normalizeWhitespace: true,
-    xmlMode: true,
-  });
-
   $('section').each((i, section) => {
     let code = 'spl_unclassified_section';
 
@@ -88,23 +104,34 @@ const populateSectionsFromXml = (xmlSplString: string, jsonResponse: OpenObject,
   });
 }
 
-const populateMetaDataFromXml = (xmlFilepath: string, jsonResponse: OpenObject) => {
-        // Process xml for meta data fields
-        const $ = cheerio.load(xmlFilepath, {
-          normalizeWhitespace: true,
-          xmlMode: true
-        });
-
+/**
+ * Iterate through the jQuery enabled SPL document and parse out top level metadata to add to the JSON response
+ * @param $: A cheerio Root section object (ie XML document in a jQuery for Node wrapper)
+ * @param jsonResponse: the in-work jsonResponse that represents the XML SPL doc but in JSON format
+ */
+const populateMetaDataFromXml = ($: cheerio.Root, jsonResponse: OpenObject) => {
         jsonResponse[`set_id`] = $('setId').attr('root');
         jsonResponse[`id`] = $('id').attr('root');
         jsonResponse[`effective_time`] = $('effectiveTime').attr('value');
         jsonResponse[`version`] = $('versionNumber').attr('value');
 }
 
+/**
+ * Parse out a provided XML SPL string into JSON based on the categories provided in the sections CSV config
+ * @param xmlSplString: A XML SPL document in string format. This could be a very large string.
+ */
 export function parse(xmlSplString: string) {
-  const jsonResponse: OpenObject = {};
-  const sectionsMap = parseSections('./sections.csv');
-  populateSectionsFromXml(xmlSplString, jsonResponse, sectionsMap);
-  populateMetaDataFromXml(xmlSplString, jsonResponse);
-  return jsonResponse;
+    const jsonResponse: OpenObject = {};
+
+    // parse the sections configuration CSV and wrap the XML SPL doc in a jQuery wrapper
+    const sectionsMap = parseSections('./sections.csv');
+    const jQueryWrappedSPLDocumentRoot = cheerio.load(xmlSplString, {
+        normalizeWhitespace: true,
+        xmlMode: true
+    });
+
+    // parse the sections/content and the top level metadata out and into the returned jsonResponse object
+    populateSectionsFromXml(jQueryWrappedSPLDocumentRoot, jsonResponse, sectionsMap);
+    populateMetaDataFromXml(jQueryWrappedSPLDocumentRoot, jsonResponse);
+    return jsonResponse;
 }
